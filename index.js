dea93e5eb0496dc62613b6991ad574cfe24fc6eb
Lucas Mombach <lucasmmombach@gmail.com>
2024-05-09 09:52:37 -0300

    Add import statements for axios, cheerio, express, and dotenv. Set up environment variable loading and basic express server configuration

---
+++ index.js
@@ -0,0 +1,45 @@
+import axios from "axios"
+import cheerio from "cheerio"
+import express from "express"
+import dotenv from "dotenv"
+
+//load environment var
+dotenv.config();
+
+const config = {
+  
+    "headers": {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
+      "Accept-Encoding": "gzip, deflate, br", 
+      "Accept-Language": "en-US,en;q=0.5", 
+      "Host": "httpbin.org", 
+      "Sec-Fetch-Dest": "document", 
+      "Sec-Fetch-Mode": "navigate", 
+      "Sec-Fetch-Site": "cross-site", 
+      "Sec-Fetch-User": "?1", 
+      "Upgrade-Insecure-Requests": "1", 
+      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0", 
+      "X-Amzn-Trace-Id": "Root=1-663cbf21-5e7f13f22874c83401bf94be"
+    }
+  };
+
+
+const url = 'https://www.amazon.com.br/s?k=liquidificador';
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+try {
+  const data = await
+      axios.get(url,config.headers);
+  const $ = cheerio.load(data);
+
+
+    
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(PORT,()=>console.log(`listing on port ${PORT}`) )
+
+
+
