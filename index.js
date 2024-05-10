6b641b6aa47cf05b0955f9357d25875ddc8d653a
Lucas Mombach <lucasmmombach@gmail.com>
2024-05-10 11:58:57 -0300

    fix axios bad request error and add simple parsing of the title

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 import cheerio from "cheerio"
-import express from "express"
+// import express from "express"
 import dotenv from "dotenv"
+import fs from "fs"
 
 //load environment var
 dotenv.config();
@@ -12,7 +13,7 @@ const config = {
       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
       "Accept-Encoding": "gzip, deflate, br", 
       "Accept-Language": "en-US,en;q=0.5", 
-      "Host": "amazon.com.br", 
+      "Host": "www.amazon.com.br", 
       "Sec-Fetch-Dest": "document", 
       "Sec-Fetch-Mode": "navigate", 
       "Sec-Fetch-Site": "cross-site", 
@@ -26,17 +27,47 @@ const config = {
 
 const url = 'https://www.amazon.com.br/s?k=liquidificador';
 const PORT = process.env.PORT || 4000;
-const app = express();
+//const app = express();
 
-try {
-  const data = await
-      axios.get(url,config.headers);
+async function fetchData(){
+  
+  try {
+    const htmlData = await
+    axios.get(url,{headers: config.headers});
+    const $ = cheerio.load(htmlData.data); 
+    let products = [];
+    products =  $('div.sg-col-4-of-24.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.s-widget-spacing-small.sg-col-4-of-20',htmlData.data)
+    .map((_, product) => { 
+      const $product = $(product); 
+      const title = $product.find('span.a-size-base-plus.a-color-base.a-text-normal').text(); 
 
-} catch (error) {
-  console.log(error);
+      // const rating = $product.find('a').text();
+      // const numberOfReviews = $product.find('a').text();
+      // const imageUrl = $product.find('a').attr('href');
+      
+      
+
+
+      // return {'title': title, 'rating': rating, 'numberOfReviews': numberOfReviews, 'imageUrl': imageUrl} 
+
+      return {'title': title};
+    }) 
+    .toArray();
+    
+    //just to see what axios is catching
+
+    // let data = htmlData.data;
+    // fs.writeFile('dataHTML.txt', data, (err) => {
+    // // In case of a error throw err.
+    //   if (err) throw err;
+    //   })
+    
+
+    console.log(products);
+
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-app.listen(PORT,() => console.log(`listing on port ${PORT}`) );
-
-
-
+fetchData();
+// app.listen(PORT,() => console.log(`listing on port ${PORT}`) );
