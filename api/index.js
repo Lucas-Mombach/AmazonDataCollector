00238fc55325e3ad7aef1874b8192f708d2637cc
Lucas Mombach <lucasmmombach@gmail.com>
2024-06-06 17:33:26 -0300

    Turn file into a Handler
    -Removes unnecessary imports
    -Removes express server
    -Change function signature to format of a handler

--- api/index.js
+++ api/index.js
@@ -1,36 +1,27 @@
 import axios from "axios"
 import cheerio from "cheerio"
-// import express from "express"
-import dotenv from "dotenv"
-import fs from "fs"
 
-//load environment var
-dotenv.config();
-
-const config = {
+export async function scrapeProducts (req, res){
+  const keyword = req.body.searchInput;
+  const config = {
+    
+      "headers": {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
+        "Accept-Encoding": "gzip, deflate, br", 
+        "Accept-Language": "en-US,en;q=0.5", 
+        "Host": "www.amazon.com.br", 
+        "Sec-Fetch-Dest": "document", 
+        "Sec-Fetch-Mode": "navigate", 
+        "Sec-Fetch-Site": "cross-site", 
+        "Sec-Fetch-User": "?1", 
+        "Upgrade-Insecure-Requests": "1", 
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0", 
+        "X-Amzn-Trace-Id": "Root=1-663cbf21-5e7f13f22874c83401bf94be"
+      }
+    };
   
-    "headers": {
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
-      "Accept-Encoding": "gzip, deflate, br", 
-      "Accept-Language": "en-US,en;q=0.5", 
-      "Host": "www.amazon.com.br", 
-      "Sec-Fetch-Dest": "document", 
-      "Sec-Fetch-Mode": "navigate", 
-      "Sec-Fetch-Site": "cross-site", 
-      "Sec-Fetch-User": "?1", 
-      "Upgrade-Insecure-Requests": "1", 
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0", 
-      "X-Amzn-Trace-Id": "Root=1-663cbf21-5e7f13f22874c83401bf94be"
-    }
-  };
-
-const keyword;
-const url = `https://www.amazon.com.br/s?k=${keyword}`;
-const PORT = process.env.PORT || 4000;
-//const app = express();
-
-async function fetchData(){
   
+  const url = `https://www.amazon.com.br/s?k=${keyword}`;
   try {
     const htmlData = await
     axios.get(url,{headers: config.headers});
@@ -43,19 +34,18 @@ async function fetchData(){
       const rating = $product.find('div.a-section.a-spacing-none.a-spacing-top-micro > div > span').attr('aria-label');
       const numberOfReviews = $product.find('div.a-section.a-spacing-none.a-spacing-top-micro > div.a-size-small').children('span').last().attr('aria-label');
       const imageUrl = $product.find('div.a-section.aok-relative.s-image-square-aspect> img.s-image').attr('src');
-      
-      
-
 
       return {'title': title, 'rating': rating, 'numberOfReviews': numberOfReviews, 'imageUrl': imageUrl} 
 
     }) 
     .toArray();
-    console.log(products);
-
+    res.json(products);
+    
   } catch (error) {
     console.log(error);
   }
+  
 }
-fetchData();
-// app.listen(PORT,() => console.log(`listing on port ${PORT}`) );
+
+
+
