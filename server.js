978111d8d63b5d51d766b93330cd7cf7685317f8
Lucas Mombach <lucasmmombach@gmail.com>
2024-06-06 16:55:39 -0300

    add middlewares and handles
    - Serve static Assets
    - Serve HTML File
    - Setup middleware
    - Setup 404 message
    - Handle /api/scrape

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const __filename = fileURLToPath(import.meta.url);
 const app = express();
 const __dirname = path.dirname(__filename);
 
+app.use(express.json());
+app.use(cors());
+app.post("/api/scrape",scrapeProducts);
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/", (req,res) => {
+  res.sendFile(__dirname + "/index.html")
+});
+
+- Serve static Assets
+- Serve HTML File
+app.use("*",(req,res) =>
+  res.status(404).json ({error:"not found"}));
+  
 const port  = 3000;
 app.listen(port,() => {
   console.log(`listening on port ${port}`);
