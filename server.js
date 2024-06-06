a110d9b437ceb7442ea57023e8144af4736548fb
Lucas Mombach <lucasmmombach@gmail.com>
2024-06-06 17:01:41 -0300

    add comments

--- server.js
+++ server.js
@@ -5,26 +5,27 @@ import path from "path";
 import { fileURLToPath } from 'url';
 
 
-const __filename = fileURLToPath(import.meta.url);
 const app = express();
+//access the current file's path and directory
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json());
+
+app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(cors());
 app.post("/api/scrape",scrapeProducts);
 
-app.use("/public", express.static(__dirname + "/public"));
-app.use("/", (req,res) => {
+app.use("/public", express.static(__dirname + "/public")); // serve static components aka index.js and styles.css
+
+app.use("/", (req,res) => { //serve the front page
   res.sendFile(__dirname + "/index.html")
 });
 
-- Serve static Assets
-- Serve HTML File
 app.use("*",(req,res) =>
   res.status(404).json ({error:"not found"}));
-  
+
 const port  = 3000;
-app.listen(port,() => {
+app.listen(port,() => { // starts server listen on port 3000
   console.log(`listening on port ${port}`);
 });
 
